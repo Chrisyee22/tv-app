7195df740f284623105c50dfc69dbd46d16d7d6f
Chris <chris.anthony.yee@gmail.com>
2019-05-10 16:21:43 -0700

    crud updates

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+
+  
   before_action :set_tv_show, only: %i[show edit update destroy]
   def index
     response = HTTParty.get("https://api.themoviedb.org/3/tv/popular?api_key=#{api_key}&language=en-US&page=1")
