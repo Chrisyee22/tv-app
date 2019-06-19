fd103511458711426f01119bfbe4f3d0a4d8841c
Chris <chris.anthony.yee@gmail.com>
2019-06-19 11:13:20 -0700

    comment out unused section

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,9 +3,7 @@ class ReviewsController < ApplicationController
 
   before_action :set_tv_show, only: %i[show edit update destroy]
   def index
-  endpoint = "https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"
-    # puts "THE MOVIE DB RESPONSE ", response
-    # @reviews = response['results'] || []
+    endpoint = "https://api.themoviedb.org/3/discover/tv?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false"
 
     response = HTTParty.get(endpoint)
     @reviews =  response['results']
@@ -14,45 +12,48 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
-  def create
-      @review = Review.new(review_params)
 
-      respond_to do |format|
-        if @review.save
-          format.html { redirect_to @review, notice: 'Tv show was successfully created.' }
-          format.json { render :show, status: :created, location: @review }
-        else
-          format.html { render :new }
-          format.json { render json: @review.errors, status: :unprocessable_entity }
-        end
-      end
-    end
+  # def create
+  #     @review = Review.new(review_params)
+  #
+  #     respond_to do |format|
+  #       if @review.save
+  #         format.html { redirect_to @review, notice: 'Tv show was successfully created.' }
+  #         format.json { render :show, status: :created, location: @review }
+  #       else
+  #         format.html { render :new }
+  #         format.json { render json: @review.errors, status: :unprocessable_entity }
+  #       end
+  #     end
+  #   end
+  #
+  #   def update
+  #     respond_to do |format|
+  #       if @review.update(review_params)
+  #         format.html { redirect_to @review, notice: 'Tv show was successfully updated.' }
+  #         format.json { render :show, status: :ok, location: @review }
+  #       else
+  #         format.html { render :edit }
+  #         format.json { render json: @review.errors, status: :unprocessable_entity }
+  #       end
+  #     end
+  #   end
+  #
+  #
+  #   def destroy
+  #     @review.destroy
+  #     respond_to do |format|
+  #       format.html { redirect_to reviews_url, notice: 'Tv show was successfully destroyed.' }
+  #       format.json { head :no_content }
+  #     end
+  #   end
 
-    def update
-      respond_to do |format|
-        if @review.update(review_params)
-          format.html { redirect_to @review, notice: 'Tv show was successfully updated.' }
-          format.json { render :show, status: :ok, location: @review }
-        else
-          format.html { render :edit }
-          format.json { render json: @review.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-
-
-    def destroy
-      @review.destroy
-      respond_to do |format|
-        format.html { redirect_to reviews_url, notice: 'Tv show was successfully destroyed.' }
-        format.json { head :no_content }
-      end
-    end
   def view_show
     show_id = request.original_url.split('/')[4]
     @review = HTTParty.get("https://api.themoviedb.org/3/tv/#{show_id}?api_key=#{api_key}&language=en-US")
     render template: 'reviews/_shows'
   end
+  
   def search_results
     search_query = request.original_url.split('/').last.match(/query=([^&]*)/)[1]
     search_endpoint = "https://api.themoviedb.org/3/search/tv?api_key=#{api_key}&language=en-US&query=#{search_query}&page=1"
